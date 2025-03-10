a05e0f01446fdee51b352c56238078a422c6fe3f
Bhuwaneshwari <bhuwaneshwari14@gmail.com>
2025-03-10 18:53:12 +0530

    0th March, Lab002_JavaBasic added

---
+++ src/ex_01_Java_Basic/Lab002_JavaBasic.java
@@ -0,0 +1,11 @@
+package ex_01_Java_Basic;
+
+public class Lab002_JavaBasic {
+    public static void main(String[] args) {
+        System.out.println(2+3);
+        System.out.println(2*3);
+        System.out.println(2/3);
+        System.out.println(2-3);
+        System.out.println(3-3);
+    }
+}
