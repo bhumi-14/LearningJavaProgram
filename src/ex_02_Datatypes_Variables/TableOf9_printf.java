5ba490227e6862f6227111d749104b09674dcffe
Bhuwaneshwari <bhuwaneshwari14@gmail.com>
2025-03-11 12:17:51 +0530

    11th March, Table using printf without for loop is added

---
+++ src/ex_02_Datatypes_Variables/TableOf9_printf.java
@@ -0,0 +1,28 @@
+package ex_02_Datatypes_Variables;
+
+public class TableOf9_printf {
+    public static void main(String[] args) {
+        byte a=1;
+        System.out.printf("%d x 9 = %d\n", a, a * 9);
+        a++;
+        System.out.printf("%d x 9 = %d\n", a, a * 9);
+        a++;
+        System.out.printf("%d x 9 = %d\n", a, a * 9);
+        a++;
+        System.out.printf("%d x 9 = %d\n", a, a * 9);
+        a++;
+        System.out.printf("%d x 9 = %d\n", a, a * 9);
+        a++;
+        System.out.printf("%d x 9 = %d\n", a, a * 9);
+        a++;
+        System.out.printf("%d x 9 = %d\n", a, a * 9);
+        a++;
+        System.out.printf("%d x 9 = %d\n", a, a * 9);
+        a++;
+        System.out.printf("%d x 9 = %d\n", a, a * 9);
+        a++;
+        System.out.printf("%d x 9 = %d\n", a, a * 9);
+
+
+    }
+}
