a54da7ce8493772878a92e5de48b44691dc6fd76
Bhuwaneshwari <bhuwaneshwari14@gmail.com>
2025-03-14 19:12:57 +0530

    14th March, Mah Function code added

---
+++ src/ex_02_Datatypes_Variables/MathFunction.java
@@ -0,0 +1,11 @@
+package ex_02_Datatypes_Variables;
+
+public class MathFunction {
+    public static void main(String[] args) {
+        int x = 13;
+        int y = 12;
+        int z = 4;
+        System.out.println(Math.sqrt((Math.pow(x,2) + Math.pow(y,2) - Math.abs(z))));
+
+    }
+}
