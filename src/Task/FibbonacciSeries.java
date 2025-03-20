9330d403424e5f439ae26da0268a7e43d54c011e
Bhuwaneshwari <bhuwaneshwari14@gmail.com>
2025-03-20 22:34:40 +0530

    20th March, code added

---
+++ src/Task/FibbonacciSeries.java
@@ -0,0 +1,28 @@
+package Task;
+
+import java.util.Scanner;
+
+public class FibbonacciSeries {
+    //Fibonacci Sequence Generator
+    //The Fibonacci Sequence is a series of numbers where each number is the sum of the two preceding ones, starting from 0 and 1.
+    //Formula:F(n)=F(n−1)+F(n−2)F(n) = F(n-1) + F(n-2)F(n)=F(n−1)+F(n−2)
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the number of terms: ");
+        int n = scanner.nextInt();
+        System.out.println("Fibonacci sequence up to " + n + " terms:");
+        for (int i = 0; i < n; i++) {
+            System.out.print(fibonacci(i) + " ");
+        }
+        scanner.close();
+    }
+    public static int fibonacci(int n) {
+        if (n == 0) {
+            return 0;
+        } else if (n == 1) {
+            return 1;
+        } else {
+            return fibonacci(n - 1) + fibonacci(n - 2);
+        }
+    }
+}
---
+++ src/ex_StringFunctions/Ex01.java
@@ -0,0 +1,13 @@
+package ex_StringFunctions;
+
+public class Ex01 {
+    public static void main(String[] args) {
+        //2. Common String Methods
+//        length()charAt(index)      substring(start, end)   contains(str) indexOf(str)
+//        lastIndexOf(str)toUpperCase()toLowerCase() trim()replace(old, new) split(regex)
+//        equals(str) equalsIgnoreCase(str)    startsWith(prefix)  endsWith(suffix)  isEmpty()
+//        join(delimiter, elements...)
+
+
+    }
+}
